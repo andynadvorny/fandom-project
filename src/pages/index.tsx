1b98e24766ef566152bdd5466bb135373f70b920
Andy Nadvorny <andynadvorny@gmail.com>
2022-09-18 11:28:05 -0300

    feat: new landing page and separated login route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,95 +1,65 @@
 import { useContext, useEffect } from 'react'
-import type { NextPage } from 'next'
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
-import { Flex, Stack, Link, Text, Button} from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
+import { Flex, Image, Box, Text, Button, Heading} from '@chakra-ui/react'
 
 import { UserContext } from '../contexts/UserContext'
-import { Input } from '../components/Form/Input'
+import { Header } from '../components/Header'
 
-const LoginFormSchema = yup.object().shape({
-  email: yup.string().required('Email is required').email('Must be a valid email address'),
-  password: yup.string().required('Password is required')
-})
-
-const Home: NextPage = () => {
+export default function Home() {
+  const { user } = useContext(UserContext)
   const router = useRouter()
-  const { user, signIn } = useContext(UserContext);
 
   useEffect(() => {
     if (user) {
       router.push('/dashboard')
     }
-  }, [user])
+  }, [])
   
-  const { register, handleSubmit, formState } = useForm({
-    resolver: yupResolver(LoginFormSchema)
-  });
-  const errors = formState.errors
-
-  function handleSignIn(data: any) {
-    signIn(data.email, data.password)
-  }
-
   return (
-    <Flex 
-      w="100vw" 
-      h="100vh" 
-      align="center" 
-      justify="center"
-      p="2"
-    >
-      <Flex
-        as="form"
-        w="full"
-        maxW={380}
-        bg="gray.800"
-        p="8"
-        borderRadius={4}
-        flexDir="column"
-        onSubmit={handleSubmit(handleSignIn)}
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex 
+        as="main"
+        w="100%"
+        maxWidth={1480}
+        h="full"
+        px="6"
+        mx="auto"
+        align="center"
+        justify={{ base: 'flex-end', md: 'space-between' }}
+        direction={{ base: 'column', md: 'row' }}
       >
-        <Stack spacing="4">
-          <Input 
-            {...register('email')}
-            name="email" 
-            type="email" 
-            label="E-mail" 
-            error={errors.email}
-          />
-          <Input 
-            {...register('password')}
-            name="password" 
-            type="password" 
-            label="Password"
-            error={errors.password}
-          />
-          <NextLink href="/forgot-password" passHref>
-            <Link fontSize="xs" textAlign="right">Forgot your password?</Link>
-          </NextLink>
-        </Stack>
-        <Button 
-          type="submit" 
-          mt="6" 
-          colorScheme="orange" 
-          isLoading={formState.isSubmitting}
+        <Flex
+          maxW={400}
+          w="full"
+          direction="column"
+          align={{ base: 'center', md: 'flex-start' }}
+          my={{ base: '8' }}
         >
-          Login
-        </Button>
-        <NextLink href="/register">
-          <Text fontSize="xs" textAlign="center" mt="4">
-            Don&apos;t have an account?{' '}
-            <Link color="orange.500" href="/register">
-              Register
-            </Link>
+          <Text>👏 Hey, welcome to</Text>
+          <Heading mt="2">
+            the <Text as="span" ml="1" color="orange.400">fandom</Text> project
+          </Heading>
+          <Text mt="2">
+            fandom made easy
           </Text>
-        </NextLink>
+          <NextLink href="/register" passHref>
+            <Button
+              colorScheme="orange" 
+              mt="6"
+            >
+              Get started
+            </Button>
+          </NextLink>
+        </Flex>
+        <Box w="100%" flex="1">
+          <Image 
+            src="/hero.svg" 
+            alt="illustration of a girl viewing multiple media forms in computer"
+          />
+        </Box>
       </Flex>
     </Flex>
   )
 }
-
-export default Home
---
+++ src/pages/login.tsx
@@ -0,0 +1,92 @@
+import { useContext, useEffect } from 'react'
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
+import { Flex, Stack, Link, Text, Button} from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { UserContext } from '../contexts/UserContext'
+import { Input } from '../components/Form/Input'
+
+const LoginFormSchema = yup.object().shape({
+  email: yup.string().required('Email is required').email('Must be a valid email address'),
+  password: yup.string().required('Password is required')
+})
+
+export default function Login() {
+  const router = useRouter()
+  const { user, signIn } = useContext(UserContext);
+
+  useEffect(() => {
+    if (user) {
+      router.push('/dashboard')
+    }
+  }, [user])
+  
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(LoginFormSchema)
+  });
+  const errors = formState.errors
+
+  function handleSignIn(data: any) {
+    signIn(data.email, data.password)
+  }
+
+  return (
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+      p="2"
+    >
+      <Flex
+        as="form"
+        w="full"
+        maxW={380}
+        bg="gray.800"
+        p="8"
+        borderRadius={4}
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Stack spacing="4">
+          <Input 
+            {...register('email')}
+            name="email" 
+            type="email" 
+            label="E-mail" 
+            error={errors.email}
+          />
+          <Input 
+            {...register('password')}
+            name="password" 
+            type="password" 
+            label="Password"
+            error={errors.password}
+          />
+          <NextLink href="/forgot-password" passHref>
+            <Link fontSize="xs" textAlign="right">Forgot your password?</Link>
+          </NextLink>
+        </Stack>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="orange" 
+          isLoading={formState.isSubmitting}
+        >
+          Login
+        </Button>
+        <NextLink href="/register">
+          <Text fontSize="xs" textAlign="center" mt="4">
+            Don&apos;t have an account?{' '}
+            <Link color="orange.500" href="/register">
+              Register
+            </Link>
+          </Text>
+        </NextLink>
+      </Flex>
+    </Flex>
+  )
+}
