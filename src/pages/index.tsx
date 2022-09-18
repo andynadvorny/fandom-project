9e5178368ff717bf580fe3a894bf0e6518c40cef
Andy Nadvorny <andynadvorny@gmail.com>
2022-09-18 10:31:01 -0300

    feat: added form handling and validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from 'next'
 import NextLink from 'next/link'
 import { Flex, Stack, Link, Text, Button} from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Input } from '../components/Form/Input'
 
+const LoginFormSchema = yup.object().shape({
+  email: yup.string().required('Email is required').email('Must be a valid email address'),
+  password: yup.string().required('Password is required')
+})
+
 const Home: NextPage = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(LoginFormSchema)
+  });
+  const errors = formState.errors
+
+  function handleSignIn(data: any) {
+    console.log(data)
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -21,17 +38,22 @@ const Home: NextPage = () => {
         p="8"
         borderRadius={4}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <Input 
+            {...register('email')}
             name="email" 
             type="email" 
             label="E-mail" 
+            error={errors.email}
           />
           <Input 
+            {...register('password')}
             name="password" 
             type="password" 
             label="Password"
+            error={errors.password}
           />
           <NextLink href="/forgot-password" passHref>
             <Link fontSize="xs" textAlign="right">Forgot your password?</Link>
@@ -41,6 +63,7 @@ const Home: NextPage = () => {
           type="submit" 
           mt="6" 
           colorScheme="orange" 
+          isLoading={formState.isSubmitting}
         >
           Login
         </Button>
