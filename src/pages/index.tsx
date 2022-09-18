fce312dd92b0d288cd651d163ff11c046a0829fc
Andy Nadvorny <andynadvorny@gmail.com>
2022-09-18 10:21:45 -0300

    feat: created initial login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,59 @@
 import type { NextPage } from 'next'
+import NextLink from 'next/link'
+import { Flex, Stack, Link, Text, Button} from '@chakra-ui/react'
+
+import { Input } from '../components/Form/Input'
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello world</h1>
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+      p="2"
+    >
+      <Flex
+        as="form"
+        w="full"
+        maxW={380}
+        bg="gray.800"
+        p="8"
+        borderRadius={4}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input 
+            name="email" 
+            type="email" 
+            label="E-mail" 
+          />
+          <Input 
+            name="password" 
+            type="password" 
+            label="Password"
+          />
+          <NextLink href="/forgot-password" passHref>
+            <Link fontSize="xs" textAlign="right">Forgot your password?</Link>
+          </NextLink>
+        </Stack>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="orange" 
+        >
+          Login
+        </Button>
+        <NextLink href="/register">
+          <Text fontSize="xs" textAlign="center" mt="4">
+            Don&apos;t have an account?{' '}
+            <Link color="orange.500" href="/register">
+              Register
+            </Link>
+          </Text>
+        </NextLink>
+      </Flex>
+    </Flex>
   )
 }
 
