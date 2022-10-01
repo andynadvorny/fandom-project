ea9542613ee6f666d6fb5b56615521529e18b1ac
Andy Nadvorny <andynadvorny@gmail.com>
2022-10-01 09:57:51 -0300

    feat: created use categories hook

---
+++ src/hooks/useCategories.tsx
@@ -0,0 +1,56 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import axios from '../api/axios';
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+interface CategoriesProviderProps {
+  children: ReactNode;
+}
+
+interface CategoriesContextData {
+  categories: Category[];
+}
+
+const CategoriesContext = createContext({} as CategoriesContextData);
+
+export function CategoriesProvider({ children }: CategoriesProviderProps) {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    getCategories()
+  }, []);
+
+  async function getCategories() {
+    try {
+      const response = await axios.get('/category')
+
+      if (response.status === 200) {
+  
+        const categoryList = response.data.body
+  
+        setCategories(categoryList)
+      }
+    } catch (e: any) {
+      const errorMessage = e.response.data == undefined ? e.message : e.response.data.message       
+
+      toast.error(errorMessage)
+    }
+  }
+
+  return (
+    <CategoriesContext.Provider value={{ categories }}>
+      {children}
+    </CategoriesContext.Provider>
+  )
+}
+
+export function useCategories() {
+  const context = useContext(CategoriesContext);
+
+  return context;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ToastContainer } from 'react-toastify'
 
 import { UserProvider } from '../contexts/UserContext'
+import { CategoriesProvider } from '../hooks/useCategories'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 
 import { theme } from '../styles/theme'
@@ -12,22 +13,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>  
       <UserProvider>
-        <SidebarDrawerProvider>
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
-          />
+        <CategoriesProvider>
+          <SidebarDrawerProvider>
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+            />
 
-          <Component {...pageProps} />
-        </SidebarDrawerProvider>
+            <Component {...pageProps} />
+          </SidebarDrawerProvider>
+        </CategoriesProvider>
       </UserProvider>
     </ChakraProvider>
   )
