16a8c161d7026e4f56b3a1ec8931e38c455af073
Andy Nadvorny <andynadvorny@gmail.com>
2022-10-08 08:43:51 -0300

    feat: created select component

---
+++ src/components/Form/Select.tsx
@@ -0,0 +1,42 @@
+import { forwardRef, ForwardRefRenderFunction } from "react"
+import { FormControl, FormErrorMessage, FormLabel, Select as ChakraSelect, SelectProps as ChakraSelectProps } from "@chakra-ui/react"
+
+interface SelectProps extends ChakraSelectProps {
+  name: string,
+  label?: string,
+  options: {
+    value: number,
+    label: string,
+  }[],
+  error: any
+} 
+
+const SelectBase: ForwardRefRenderFunction<HTMLSelectElement, SelectProps> 
+  = ({ name, label, options, error, ...rest }, ref) => {
+    return (
+      <FormControl isInvalid={!!error}>
+        { label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+        <ChakraSelect 
+          placeholder=" "
+          name={name} 
+          id={name} 
+          focusBorderColor="orange.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgCoolor: "gray.900"
+          }}
+          ref={ref}
+          {...rest}
+        >
+          {options.map( option => (
+            <option value={option.value} key={option.value}>{option.label}</option>
+          ))}
+        </ChakraSelect>
+
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      </FormControl>
+    )
+}
+
+export const Select = forwardRef(SelectBase)
